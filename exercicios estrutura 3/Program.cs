a5b4daf4366f5a13d974185d2eae988df418ecc8
vitor <vitor@DESKTOP-4OIHHQ9>
2023-04-26 16:05:50 -0300

    Add project files.

---
+++ exercicios estrutura 3/Program.cs
@@ -0,0 +1,18 @@
+﻿float A = float.Parse(Console.ReadLine());
+float B = float.Parse(Console.ReadLine());
+float C = float.Parse(Console.ReadLine());
+float triangulo, circulo, trapezio, quadrado, retangulo;
+
+triangulo = (float)((A * C) / 2);
+circulo = (float)((C * C) * Math.PI);
+trapezio = (float)(((A + B) * C) / 2);
+quadrado = (float)(B * B);
+retangulo = (float)(A * B);
+
+Console.WriteLine(A + " " + B + " " + C);
+
+Console.WriteLine("Triangulo: " + triangulo.ToString("F3"));
+Console.WriteLine("Circulo: " + circulo.ToString("F3"));
+Console.WriteLine("Trapezio: " + trapezio.ToString("F3"));
+Console.WriteLine("Quadrado: " + quadrado.ToString("F3"));
+Console.WriteLine("Retangulo: " + retangulo.ToString("F3"));
